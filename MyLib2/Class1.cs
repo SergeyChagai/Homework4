e0797ef0682fc53ff19566e12b22218b659d67d9
Serjo <Serjo@DESKTOP-TRAVELM>
2020-10-29 16:06:53 +0300

    reference

---
+++ MyLib2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MyLib2
+{
+    public static class Cycle
+    {
+        public static int Degree(int a)
+        {
+            int degree = 1;
+            for (int i = 0; i < a; i++)
+                degree = degree * a;
+            return degree;
+        }
+    }
+}
